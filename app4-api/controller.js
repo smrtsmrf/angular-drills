01d69badbdee680f1de88f0fa02e7393c707dc7c
Candice Humpherys <smrtsmrf@gmail.com>
2016-07-07 20:01:12 -0600

    working. just some small kinks and styles to mess with

--- app4-api/controller.js
+++ app4-api/controller.js
@@ -1,33 +1,64 @@
 angular.module('apiApp').controller('apiCtrl', ['$scope', 'apiSvc', function ($scope, apiSvc) {
+	$scope.shown = true;
 
-	$scope.getPokemon = function () {
-		apiSvc.getAllPokemon().then(function(pokemon) {
-			$scope.pokemon = pokemon;
+	// $scope.sortProp = 'Name';
+
+	$scope.filterOptions = {
+    		filterText: ''
+  	};
+
+	$scope.getPokemon = function (offset) {
+		apiSvc.getAllPokemon(offset).then(function(pokemon) {
+			if (offset === 0) {
+				$scope.pokemon = pokemon;
+			}
+			else if (offset === 181) {
+				$scope.pokemon2 = pokemon;
+			}
+			else if (offset === 361) {
+				$scope.pokemon3 = pokemon;
+			}
+			else if (offset === 541) {
+				$scope.pokemon4 = pokemon;
+			}
 		})
 	}
-	$scope.getPokemon();
 
+	$scope.getPokemon(0);
+	$scope.getPokemon(181);
+	$scope.getPokemon(361);
+	$scope.getPokemon(541);
 
-	$scope.getPokeInfo = function (pokemonInfo) {
-		apiSvc.getPokeInfo(pokemonInfo).then(function(response) {
+	var dataArray = ['pokemon', 'pokemon2', 'pokemon3', 'pokemon4'];
+	var off = [1, 182, 362, 542];
+
+	$scope.gridOptions = [];
+	for (var i = 0; i < dataArray.length; i++) {
+		var idx = '<div class="ngCellText" data-ng-class="col.colIndex()"><span>{{row.rowIndex ' + '+' +off[i]+'}}</span></div>';
+		$scope.gridOptions.push(
+			{enableColumnResize: true,
+			enableSorting: true,
+			// sortInfo: {fields: ['Name', '#'], directions: ['asc']},
+			filterOptions: $scope.filterOptions,
+		      data: dataArray[i],
+		      jqueryUITheme: true,
+		      enableRowSelection: false,
+		      height: '110px',
+		      columnDefs: [
+		      {displayName:'#', width: '50px', cellTemplate: idx, cellClass: 'grid-align', pinned: true},
+		        {field: 'name', displayName: 'Name', width: '145px', cellClass: 'grid-align', pinned: true},
+		        {field: 'url', displayName: 'Get More Info', width: '150px',cellTemplate: '<button ng-click="getPokeInfo(row.getProperty(col.field))">More</button>', cellClass: 'grid-align', pinned: true},
+		      ]})
+	};
+
+	$scope.getPokeInfo = function (pokemonInfoURL) {
+		$scope.shown = !$scope.shown;
+		apiSvc.getPokeInfo(pokemonInfoURL).then(function(response) {
 			$scope.pokemonInfo = response.data;
-			console.log($scope.pokemonInfo);
+			console.log(response.data);
+			
 		})
 	}
-
-	$scope.gridOptions = { 
-	      enableColumnResize: true,
-	      data: 'pokemon',
-	      // filterOptions: $scope.filterOptions,
-	      // filterOptions: {filterText: '', useExternalFilter: false},
-	      // showFilter: true,
-	      height: '110px',
-	      // sortInfo: {fields: ['Song', 'Artist', 'Collection', 'Type', 'Genre', 'TrackPrice', 'CollectionPrice', 'Explicit'], directions: ['asc']},
-	      columnDefs: [
-	        {field: 'name', displayName: 'Name'},
-	        {field: 'url', displayName: 'API url'},
-	      ]
-  };
 
 	
 	
@@ -36,4 +67,3 @@ angular.module('apiApp').controller('apiCtrl', ['$scope', 'apiSvc', function ($s
 
 
 
-// ng-grid like itunes to display
